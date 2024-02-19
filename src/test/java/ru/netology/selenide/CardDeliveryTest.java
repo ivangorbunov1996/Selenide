0348bf81a50cff4637e2f0a54bb80bb1a66ea180
SolaRich <ivan100500707@gmail.com>
2024-02-19 22:02:54 +0300

    add test

---
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -0,0 +1,42 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @BeforeEach
+    public void setupTest() {
+        open("http://localhost:9999");
+    }
+
+    private String dateGeneration(int addedDays) {
+        LocalDate date = LocalDate.now();
+        LocalDate newDate = date.plusDays(addedDays);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateAdd = newDate.format(dateTimeFormatter);
+        return dateAdd;
+    }
+
+    @Test
+    void shouldRegisterFormTest() {
+        $("[data-test-id='city'] input").setValue("Ярославль");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(dateGeneration(5));
+        $("[data-test-id='name'] input").setValue("Горбунов Иван");
+        $("[data-test-id='phone'] input").setValue("+79109691089");
+        $("[data-test-id='agreement'] span.checkbox__box").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateGeneration(5)));
+    }
+}
